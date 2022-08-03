85ed7cb546d42fb6e03f6463e59b3b28ee73be52
Adair Juneo <adair_juneo@hotmail.com>
2022-08-03 17:45:44 -0300

    Validações e reactt-hook-form em funcionamento.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { FiPlay } from 'react-icons/fi'
 import {
   TaskInput,
@@ -10,19 +13,46 @@ import {
   StartCountdownButton,
 } from './styles'
 
+const newCycleSchemaFormValidations = zod.object({
+  task: zod
+    .string()
+    .min(3, { message: 'O mínimo de caracteres é 3.' })
+    .max(25, { message: 'O máximo de caracteres é 25.' }),
+  minutesAmount: zod
+    .number()
+    .min(5, { message: 'O valor mínimo de minutos é 5.' })
+    .max(60, { message: 'O valor máximo de minutos é 60.' }),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleSchemaFormValidations>
+
 export const Home: React.FC = () => {
+  const { register, handleSubmit, formState, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleSchemaFormValidations),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { errors, isSubmitting } = formState
+
+  const createNewCycle = (data: NewCycleFormData) => {
+    console.log(data)
+    reset()
+  }
+
   return (
     <HomeContainer>
-      <form action='#'>
+      <form action='' onSubmit={handleSubmit(createNewCycle)}>
         <FormContainer>
           <label htmlFor='task'>Vou trabalhar em</label>
           <TaskInput
             id='task'
             type='text'
-            required
-            minLength={5}
-            placeholder='Dê um nome para essa tarefa'
             list='taskSuggestions'
+            placeholder='Dê um nome para essa tarefa'
+            {...register('task')}
           />
           <datalist id='taskSuggestions'>
             <option value='Tarefa 01' />
@@ -36,14 +66,10 @@ export const Home: React.FC = () => {
           <label htmlFor='minutesAmount'>durante</label>
           <MinutesAmountInput
             id='minutesAmount'
-            list='minutesOptions'
             type='number'
             placeholder='00'
-            required
-            min={5}
-            max={60}
-            step={5}
-            maxLength={2}
+            list='minutesOptions'
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <datalist id='minutesOptions'>
             <option value={5} />
@@ -56,6 +82,8 @@ export const Home: React.FC = () => {
 
           <span>minutos</span>
         </FormContainer>
+        {errors.task && <span>{errors.task?.message}</span>}
+        {errors.minutesAmount && <span>{errors.minutesAmount?.message}</span>}
 
         <CountdownContainer>
           <span>0</span>
@@ -65,7 +93,7 @@ export const Home: React.FC = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type='submit' disabled={false}>
+        <StartCountdownButton type='submit' disabled={isSubmitting}>
           <FiPlay size={24} />
           Começar
         </StartCountdownButton>
